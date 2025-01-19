62a16c53e9ca5c89cda44472690c3e7472737a4f
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2025-01-19 19:37:38 +0100

    Changing the epi endpoint

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.express as px
 
 # Backend API URL
-API_BASE_URL = "http://127.0.0.1:5000/api"
+API_BASE_URL = "http://127.0.0.1:5000"
 
 # Fetch financial data
 @st.cache
